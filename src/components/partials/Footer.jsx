f0b8a84514f9c31e146e426d91a15c1f1be10001
Chanpasong Thipphakhinkeo <Chanpasong.Thipphakhinkeo@gmail.com>
2026-03-13 14:36:28 -0400

    adjusted mobile styles, added sound, adjusting switch

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -3,7 +3,9 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <p>© 2026 · Chanpasong Oscar Thipphakhinkeo · All rights reserved</p>
+      <p className="mr-4">
+        © 2026 · Chanpasong Oscar Thipphakhinkeo · All rights reserved
+      </p>
       <p>Be kind and smile everyday</p>
     </footer>
   );
